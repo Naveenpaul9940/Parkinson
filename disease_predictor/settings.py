5214b21dd0b17c24ccb0014ebd14e3f33a350461
naveen <naveen.r.2021.ad@ritchennai.edu.in>
2025-04-11 17:29:31 +0530

    Fix for Vercel deployment

--- disease_predictor/settings.py
+++ disease_predictor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#ffbmzvf6koaivhk&0cmt@+yh6nvt%o63ocih*#_fmbibg7)3i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-vercel-project.vercel.app', '127.0.0.1']
 
 
 # Application definition
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 import os
 
